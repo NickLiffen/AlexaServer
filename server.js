04e6e877d5e165038f28ce0cdc78277edc2476b1
NickLiffen <UP653591@myport.ac.uk>
2016-12-11 14:56:31 +0000

    Added Comments

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 
 'use strict';
 
+// Requring the Amazon Alexa Server Module.
 var AlexaAppServer = require('alexa-app-server');
+
+// Starting the Amazon Alexa Server; it runs similar to Express.
 AlexaAppServer.start( {port:process.env.PORT || 8080} );
