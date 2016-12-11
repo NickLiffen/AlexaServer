9588411d09004ce999acdf6d230f61e514eb12bb
NickLiffen <UP653591@myport.ac.uk>
2016-12-11 16:00:04 +0000

    Adding Alexa Skill

--- server.js
+++ server.js
@@ -4,5 +4,10 @@
 // Requring the Amazon Alexa Server Module.
 var AlexaAppServer = require('alexa-app-server');
 
-// Starting the Amazon Alexa Server; it runs similar to Express.
-AlexaAppServer.start( {port:process.env.PORT || 8080} );
+AlexaAppServer.start({
+    server_root:__dirname,        // Path to root
+    public_html:"public_html",    // Static content
+    app_dir:"apps",               // Where alexa-app modules are stored
+    app_root:"/alexa/",           // Service root
+    port:process.env.PORT || 8080 // What port to use, duh
+});
